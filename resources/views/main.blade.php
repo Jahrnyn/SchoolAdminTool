dcf9d80c3283181838061cfa8b86046b8ffe10b6
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-06-21 14:07:59 +0200

    Layout created for header and footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page</title>
-    <link rel="stylesheet" href="/main.css">
-</head>
-<body>
-    <header class="header">
-        <div class="header-left">
-            <h1>Székelybertalanban Álltalános Iskola</h1>
-        </div>
-        <div class="header-right">
-            <form class="login-form">
-                <input type="email" name="email" placeholder="Email Address">
-                <input type="password" name="password" placeholder="Password">
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </header>
+<x-layout>
 
     <main>
         <section>
@@ -26,11 +6,6 @@
             <p>This is a simple school admin tool that helps manage student information.</p>
         </section>
     </main>
+    
+</x-layout>
 
-    <footer class="footer">
-        <p>&copy; 2023 Székelybertalan. All rights reserved.</p>
-    </footer>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
--- resources/views/student-management.blade.php
+++ resources/views/student-management.blade.php
@@ -1,35 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page</title>
-    <link rel="stylesheet" href="/main.css">
-</head>
-<body>
-    <header class="header">
-        <div class="header-left">
-            <h1>Székelybertalanban Álltalános Iskola</h1>
-        </div>
-        <div class="header-right">
-            <form class="login-form">
-                <input type="email" name="email" placeholder="Email Address">
-                <input type="password" name="password" placeholder="Password">
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </header>
+<x-layout>
 
-    <main>
-        <section>
-            <h2>Ide fog kerülni a diákok listája az adatbázisból</h2>
-        </section>
-    </main>
+<main>
+    <section>
+        <h2>Ide fog kerülni a diákok listája az adatbázisból</h2>
+    </section>
+</main>
 
-    <footer class="footer">
-        <p>&copy; 2023 Székelybertalan. All rights reserved.</p>
-    </footer>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+</x-layout>
